c508c15bf6fdade58008c8d3d0aa449129a52db1
crangoren <crangoren@gmail.com>
2021-11-11 15:51:28 +0300

    add hw1

---
+++ src/main/java/Products.java
@@ -0,0 +1,11 @@
+public class Products {
+    int id;
+    String name;
+    double cost;
+
+    public Products(int id, String name, double cost) {
+        this.id = id;
+        this.name = name;
+        this.cost =  cost;
+    }
+}
