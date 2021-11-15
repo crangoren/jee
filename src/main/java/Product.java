c0f820c659ec80ec431a7839d4ea2d4851d24bd0
crangoren <crangoren@gmail.com>
2021-11-15 20:03:46 +0300

    fix hw 1.1

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -3,9 +3,33 @@ public class Product {
     String name;
     double cost;
 
-    public Product(int id, String name, double cost) {
+    Product(int id, String name, double cost) {
         this.id = id;
         this.name = name;
         this.cost =  cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
     }
 }
